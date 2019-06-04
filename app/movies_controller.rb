1fc22142b16ea41efdee4cdca31298d925cbdbf3
Brian Simonton <bsimonton78@gmail.com>
2019-06-04 15:42:00 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -33,13 +33,20 @@ end
     #m.release_date = 1990
  # end
 
-  def can_be_created_in_a_block
-  Movie.create do |m|
-    m.title = "Home Alone"
-    m.release_date = 1990
-  end
-end
+def can_be_created_in_a_block(args = {title: "Home Alone", release_date: "1990"})
+  # If no arguments are passed, use default values:
+  # title == "Home Alone"
+  # release_date == 1990
+  
 
+  Movie.create do |m|
+    __
+    
+    m.title = args[:title]
+    m.release_date  = args[:release_date]
+  end
+  
+end
 
 
 def can_get_the_first_item_in_the_database
